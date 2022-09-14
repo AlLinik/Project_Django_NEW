1dd534ff356c221a66b97eeeba938c720f762754
HP <terlin@mail.ru>
2022-09-15 00:39:46 +0300

    project_new_15_09_2022

--- my_project_1/my_project_1/views.py
+++ my_project_1/my_project_1/views.py
@@ -1,10 +1,3 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
-def about(request):
-    return render(request, 'about.html')
-
-
-def home(request):
-    return HttpResponse('HOME')
