403bc6576ca37ce07d336200678f06b462b80bac
HP <terlin@mail.ru>
2022-09-16 01:28:22 +0300

    project_new_16_09_2022

--- my_project_1/my_project_2_app/views.py
+++ my_project_1/my_project_2_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Task
 
 
@@ -10,3 +9,4 @@ def index(request):
 
 def about(request):
     return render(request, 'app/about.html')
+
